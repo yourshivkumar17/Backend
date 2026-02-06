9463d9a34cf53a351bfce25b6fda57889f26ed58
Shiv kumar <yourshivkumar17@gmail.com>
2026-02-06 14:27:10 +0530

    httpsServer

---
+++ SelfPractice/CreatehttpserverWithTheHelpOfNode.js/CreatehttpServerNode.js
@@ -0,0 +1,12 @@
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    res.end("Hello CBT Exam!\nWelcome to Node.js HTTP Server");
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
