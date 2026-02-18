89a630af7809bef9f4971fed5c6f088cf555d2a2
Shiv kumar <yourshivkumar17@gmail.com>
2026-02-18 10:33:53 +0530

    MethodFindServer

---
+++ ClassWork/FindMethodLikeGetPutDelete/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const logPath = path.join(__dirname, "log.txt");
+
+const server = http.createServer((req, res) => {
+
+    console.log("Request mili:", req.url);
+    console.log("Log file path:", logPath);
+
+    const log = new Date().toISOString() + " " + req.method + " " + req.url + "\n";
+
+    fs.appendFileSync(logPath, log);
+
+    res.end("OK");
+});
+
+server.listen(3000, () => {
+    console.log("Server Running...");
+});
