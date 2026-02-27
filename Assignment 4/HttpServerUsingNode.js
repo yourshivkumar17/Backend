cdb984f90e800d0216f74942acb623b48503811c
Shiv kumar <yourshivkumar17@gmail.com>
2026-02-27 16:25:10 +0530

    httpservernodejs

---
+++ Assignment 4/HttpServerUsingNode.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req,res) => {
+     res.writeHead(200, {"Content-Type":"text/plain"});
+     res.end("Hello World\nMy Name Is Shiv");
+});
+
+server.listen(3000, () => {
+console.log("Server is Running");
+});
--- ClassWork/FindMethodLikeGetPutDelete/server.js
+++ ClassWork/FindMethodLikeGetPutDelete/server.js
@@ -6,13 +6,13 @@ const logPath = path.join(__dirname, "log.txt");
 
 const server = http.createServer((req, res) => {
 
-    console.log("Request mili:", req.url);
+    console.log("Request find:", req.url);
     console.log("Log file path:", logPath);
 
     const log = new Date().toISOString() + " " + req.method + " " + req.url + "\n";
 
     fs.appendFileSync(logPath, log);
-
+ 
     res.end("OK");
 });
 
