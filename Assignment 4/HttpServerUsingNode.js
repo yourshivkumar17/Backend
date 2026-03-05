1a17be6d03ea1f975ee6f913d3a5c1f29dfb4c70
Shiv kumar <yourshivkumar17@gmail.com>
2026-03-05 15:00:14 +0530

    change

--- Assignment 4/HttpServerUsingNode.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const server = http.createServer((req,res) => {
-     res.writeHead(200, {"Content-Type":"text/plain"});
-     res.end("Hello World\nMy Name Is Shiv");
-});
-
-server.listen(3000, () => {
-console.log("Server is Running");
-});
